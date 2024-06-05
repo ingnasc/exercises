be1a741b9cec4a1657bceb044b50eef3c22688b2
Ingrid Nascimento <ingcnasc@gmail.com>
2024-06-04 21:19:11 -0300

    second task

---
+++ javascript_activities/variables/task_02.js
@@ -0,0 +1,11 @@
+//Crie duas variáveis, uma contendo seu primeiro nome e outra contendo seu último nome. Em seguida, combine-as em uma terceira variável usando o operador + e em uma quarta variável usando template strings. Por fim, imprima os resultados obtidos no console.
+
+const nome = 'Ingrid';
+const sobrenome = 'Nascimento';
+const nomesJuntos = nome + ' ' + sobrenome;
+const nomeEmTexto = `O seu nome e sobrenome é ${nomesJuntos}.`;
+
+console.log(nome);
+console.log(sobrenome);
+console.log(nomesJuntos);
+console.log(nomeEmTexto);
