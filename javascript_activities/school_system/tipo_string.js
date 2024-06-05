4aceb0ff4e07cdf76399d1d34843b459c585c702
Ingrid Nascimento <ingcnasc@gmail.com>
2024-06-05 11:44:29 -0300

    learning about string

---
+++ javascript_activities/school_system/tipo_string.js
@@ -0,0 +1,9 @@
+const estudante = "Ingrid";
+const docente = 'Ana';
+
+const cumprimento = "Nosso lema 'Estudar Bastante!'."
+
+const citacao = `Ju diz: ${cumprimento}`;
+
+console.log(cumprimento);
+console.log(citacao);
