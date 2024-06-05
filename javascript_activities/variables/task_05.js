8b6c82ca42757825ec3d9e2d6ff51a21283929e7
Ingrid Nascimento <ingcnasc@gmail.com>
2024-06-04 21:36:06 -0300

    error correction

--- javascript_activities/variables/task_05.js
+++ javascript_activities/variables/task_05.js
@@ -1,8 +1,12 @@
 //Declare uma variável usando var fora de um bloco de código (por exemplo, if) e outra dentro desse bloco. Tente acessar essas variáveis dentro do bloc e fora dele utilizando console.log e analise os resultados. Faça o mesmo processo utilizando let e compare com os resultados anteriores.
 
-let variavel = 5;
+let fora = 5;
 
-if(variavel === 5) {
-variavel = 6;
-console.log(variavel);
+if(true) {
+    let dentro = 6;
+    console.log(fora);
+    console.log(dentro);
 }
+
+console.log(fora);
+console.log(dentro);
