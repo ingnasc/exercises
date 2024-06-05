2f0f17f9c376531648dbb55ddb1d4fd40f30706d
Ingrid Nascimento <ingcnasc@gmail.com>
2024-06-05 11:51:43 -0300

    learning about boolean

---
+++ javascript_activities/school_system/tipo_boolean.js
@@ -0,0 +1,8 @@
+const estudante = 'Fernando';
+const aprovacao = true;
+
+if(aprovacao === true) {
+    console.log('Parabéns, você foi aprovado!');
+} else {
+    console.log('Você está de recuperação.');
+}
