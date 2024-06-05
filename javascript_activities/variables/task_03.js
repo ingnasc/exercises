c809f4f2585ccf864f8e99a970462da27d954831
Ingrid Nascimento <ingcnasc@gmail.com>
2024-06-04 21:23:10 -0300

    third task

---
+++ javascript_activities/variables/task_03.js
@@ -0,0 +1,7 @@
+//Declare duas variáveis, uma contendo um número e outra contendo uma string. Em seguida, combine-as em uma terceira variável usando template strings para montar uma frase e exiba o resultado no console.
+
+const numero = 19;
+const texto = 'Texto Aqui';
+const juncaoNomeTexto = numero + ' ' + texto;
+
+console.log(`A união das variáveis é ${juncaoNomeTexto}.`);
