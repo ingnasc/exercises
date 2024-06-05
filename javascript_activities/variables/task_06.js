8a0c8b52e5323f2dc8ff81e9265c23825ff027e0
Ingrid Nascimento <ingcnasc@gmail.com>
2024-06-04 21:43:39 -0300

    sixth activity

---
+++ javascript_activities/variables/task_06.js
@@ -0,0 +1,9 @@
+//Declare uma variável booleana que informa se está chovendo e utilize-a em uma estrutura condicional (if) para exibir uma mensagem informando se é preciso levar um guarda-chuva ou não dependendo do valor da variável.
+
+let estaChovendo = true;
+
+if(estaChovendo) {
+    console.log('Leve um guarda-chuva');
+} else {
+    console.log('Não precisa de um guarda-chuva');
+}
