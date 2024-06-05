90d57ccea0a0377a960b7b9d22e95e4e976d562d
Ingrid Nascimento <ingcnasc@gmail.com>
2024-06-04 21:15:27 -0300

    tasks

---
+++ javascript_activities/variables/task_01.js
@@ -0,0 +1,9 @@
+//Declare três variáveis diferentes (uma para cada tipo: string, número e booleano) e atribua valores a elas. Em seguida, exiba o tipo de cada variável no console.
+
+const texto = 'Olá, mundo!';
+const numero = 2;
+const booleano = true;
+
+console.log(typeof texto);
+console.log(typeof numero);
+console.log(typeof booleano);
