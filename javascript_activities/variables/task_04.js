46d56e6e9e96ae720d67ac39a320d516ad158e9e
Ingrid Nascimento <ingcnasc@gmail.com>
2024-06-04 21:26:00 -0300

    fourth task

---
+++ javascript_activities/variables/task_04.js
@@ -0,0 +1,7 @@
+//Crie uma variável inicializada com um valor de qualquer tipo e, em seguida, reatribua um novo valor a essa variável. Imprima ambos os valores no console após atribuí-los.
+
+let variavel = 5;
+console.log(variavel);
+
+variavel = 11;
+console.log(variavel);
