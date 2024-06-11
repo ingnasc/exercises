ec85b9d2930cc1d7a62ba27dd3946afe34e14a15
Ingrid Nascimento <ingcnasc@gmail.com>
2024-06-11 16:41:22 -0300

    learning about truthy or falsy

--- javascript_activities/school_system/operadores_logicos.js
+++ javascript_activities/school_system/operadores_logicos.js
@@ -1,8 +1,15 @@
 const notaFinal = 7;
 const faltas = 3;
+const advertencias = 0; //seria interpretado como falso no if
 
 if(notaFinal < 7 && faltas > 4) { 
     console.log('Reprovado, boas festas!');
 } else {
     console.log('Aprovado, boas festas!');
 }
+
+if(faltas >=2 && !advertencias) { //como se fosse uma dupla negacao
+        console.log('Recebeu bônus!');
+    } else {
+        console.log('Não recebeu bônus!');
+    }
