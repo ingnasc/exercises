aff9835bd51f0f96e2fc895fa2dcbb5f77a44f5a
Ingrid Nascimento <ingcnasc@gmail.com>
2024-06-11 15:46:53 -0300

    fourth task

---
+++ javascript_activities/types_of_data/task04.js
@@ -0,0 +1,10 @@
+//Crie uma variável numérica e uma string. Faça a conversão da variável numérica para string e da string para número. Exiba os tipos de dados resultantes no console.
+
+const numero = 45;
+const texto = '569';
+
+const conversaoNumero = Number(texto);
+const conversaoTexto = String(numero);
+
+console.log(typeof conversaoNumero);
+console.log(typeof conversaoTexto);
