ecc6185b531487a2af6a33b63bb37966e2154a8b
Ingrid Nascimento <ingcnasc@gmail.com>
2024-06-11 15:40:51 -0300

    third task

---
+++ javascript_activities/types_of_data/task03.js
@@ -0,0 +1,7 @@
+//Crie 3 variáveis de tipos diferentes, utilize template strings para combinar os diferentes tipos de dados (number, string, boolean) em uma única string e exiba o resultado no console.
+
+const numero = 2.839;
+const texto = 'Texto aqui.';
+const booleano = true;
+
+console.log(`${numero} ${texto} ${booleano}`);
