9317a8deb5d68a7798009217a62c14b3dce9e23d
Ingrid Nascimento <ingcnasc@gmail.com>
2024-06-11 15:22:08 -0300

    first task

---
+++ javascript_activities/types_of_data/task01.js
@@ -0,0 +1,5 @@
+//Crie uma variável contendo uma frase. Em seguida, exiba no console o comprimento da frase e a mesma frase com letras maiúsculas.
+
+const frase = 'A frase aqui.';
+
+console.log(frase.toUpperCase());
