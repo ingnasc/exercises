c26a2c1e3f3cf919ad4ca466cbab34cf6f2050e1
Ingrid Nascimento <ingcnasc@gmail.com>
2024-06-11 16:48:59 -0300

    learning about operators and comparisons

---
+++ javascript_activities/school_system/operadores_comparacoes.js
@@ -0,0 +1,11 @@
+const estaAprovado = true;
+
+if(estaAprovado === true) {
+    console.log('Está aprovado!');
+}
+
+if('0' === 0) { //== compara valor, === compara valor e tipo, sempre usar esse
+    console.log('Passou na comparação.');
+} else {
+    console.log('Não passou na comparação.');
+}
