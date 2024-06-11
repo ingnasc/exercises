17255fb99e9dcc4ba21526bd1c5cc3ec988d2b0b
Ingrid Nascimento <ingcnasc@gmail.com>
2024-06-11 15:56:06 -0300

    fifth task

---
+++ javascript_activities/types_of_data/task05.js
@@ -0,0 +1,8 @@
+//Crie uma variável com uma string e utilize métodos de manipulação de strings, como toUpperCase, toLowerCase, slice ou outros, para modificar a string original. Exiba os resultados finais no console.
+
+const texto = "Texto Aqui";
+
+console.log(texto.toUpperCase() + ' para deixar em letras maiúsculas.');
+console.log(texto.toLowerCase() + ' para deixar em letras minúsculas.');
+console.log(texto.length + ' para saber a quantidade de caracteres.');
+console.log(texto.slice(0, 5) + ' para retornar apenas uma parte da string.');
