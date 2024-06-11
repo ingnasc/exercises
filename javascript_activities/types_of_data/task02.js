3a66185444a88685d021ce3ed6a6e172992b1c1a
Ingrid Nascimento <ingcnasc@gmail.com>
2024-06-11 15:28:08 -0300

    second task

---
+++ javascript_activities/types_of_data/task02.js
@@ -0,0 +1,6 @@
+//Declare duas variáveis, uma inicializada com valor null e outra que não recebe atribuição (resultando em undefined). Exiba os valores no console.
+
+const variavelUm = null;
+let variavelDois; //só let pode fazer isso
+
+console.log(variavelUm + ' ' + variavelDois);
