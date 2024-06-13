fa92e08f6ce5ec0acaeeebd58dbf4abe0bf20362
Ingrid Nascimento <ingcnasc@gmail.com>
2024-06-13 18:17:56 -0300

    second task

---
+++ javascript_activities/operators/task02.js
@@ -0,0 +1,7 @@
+//Crie uma variável numérica com um valor. Utilize um operador ternário para verificar se esse valor é par ou ímpar. Exiba o resultado no console.
+
+const numero = 52;
+
+const resultado = (numero % 2) === 0 ? 'Par' : 'Ímpar'; // % restante
+
+console.log(resultado);
