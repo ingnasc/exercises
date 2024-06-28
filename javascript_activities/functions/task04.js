8e871f2e6bc5b606ba166f6c6694f51d62678cfe
Ingrid Nascimento <ingcnasc@gmail.com>
2024-06-27 21:33:09 -0300

    fifth task

---
+++ javascript_activities/functions/task04.js
@@ -0,0 +1,13 @@
+//Crie uma função que receba três números como parâmetros e determine qual é o maior entre eles. Utilize estruturas condicionais (if, else) para comparar os valores e determinar o maior. Imprima o maior valor no console.
+
+function ordenaNumeros(num1, num2, num3) {
+    if(num1 > num2 && num1 > num3) {
+        console.log(num1);
+    } else if (num2 > num1 && num2 > num3) {
+        console.log(num2);
+    } else {
+        console.log(num3);
+    }
+}
+
+ordenaNumeros(2,5,8);
