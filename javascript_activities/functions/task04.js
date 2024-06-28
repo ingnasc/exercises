7965a1573a83dc9267cc7717de68a1b5ae223ce4
Ingrid Nascimento <ingcnasc@gmail.com>
2024-06-27 21:35:52 -0300

    easy way

--- javascript_activities/functions/task04.js
+++ javascript_activities/functions/task04.js
@@ -11,3 +11,20 @@ function ordenaNumeros(num1, num2, num3) {
 }
 
 ordenaNumeros(2,5,8);
+
+//easy way
+//function encontraMaiorValor(num1, num2, num3) {
+//    let maior = num1; // Assumindo inicialmente que num1 é o maior
+  
+//    if (num2 > maior) {
+//        maior = num2;
+//    }
+  
+//    if (num3 > maior) {
+//        maior = num3;
+//    }
+  
+//   return maior;
+//  }
+  
+//  console.log(encontraMaiorValor(12, 56, 32)); 
