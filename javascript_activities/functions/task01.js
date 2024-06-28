1275735cd5888c296f74b2e43f15d31f62a519f2
Ingrid Nascimento <ingcnasc@gmail.com>
2024-06-27 21:14:44 -0300

    first task

---
+++ javascript_activities/functions/task01.js
@@ -0,0 +1,10 @@
+//Crie uma função que receba o nome de uma pessoa como argumento e retorne uma saudação personalizada. Em seguida, chame a função e exiba a saudação no console.
+
+function saudacao(nome) {
+    return console.log(`Olá, ${nome}, tudo bem?`);
+}
+
+const nomePessoa = 'Ingrid';
+const mensagem = saudacao(nomePessoa);
+
+console.log(saudacao);
